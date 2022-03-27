25ea6bdff9675ba3e34011e9c908c6266a42a2a3
Kuba <jkurczewski@edu.cdv.pl>
2022-03-27 08:30:48 +0200

    Courses 4 & 5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import { logger } from 'src/logger';
+import { startServer } from 'src/server';
+import config from 'src/config';
+
+logger.info('Hello info!');
+
+startServer({
+    port: config.options.port,
+    corsOptions: config.options.cors,
+});
